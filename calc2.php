9811dc785d115a2665b1da772909a2bd3eb6b993
lexa <alex.n.prdk@gmail.com>
2017-03-30 14:25:30 +0300

    33

--- calc2.php
+++ calc2.php
@@ -17,8 +17,8 @@ require_once 'function/calcFunction.php';
         
         <form action="calc2.php" method="POST" name="muform" align="center">
             
-            <input type="text" placeholder="Введите число1" 
-                   name="n_1" title="Число1" value="<?php //echo $n_1; ?>" >
+            <input type="text" placeholder="Введите число1" autocomplete="off"
+                   name="n_1" title="Число1" value="<?php echo $n_1; ?>" >
             <select name="operations">
                 <option>+</option>
                 <option>-</option>
@@ -26,8 +26,8 @@ require_once 'function/calcFunction.php';
                 <option>/</option>
                 
             </select>
-            <input type="text" placeholder="Введите число2" 
-                   name="n_2" title="Число2" value="<?php //echo $n_2; ?>"> = 
+            <input type="text" placeholder="Введите число2" autocomplete="off"
+                   name="n_2" title="Число2" value="<?php echo $n_2; ?>"> = 
             <input type="text" name="result" size="30" readonly="" 
                    value="<?php  echo " $result "; ?>">
             <p>
