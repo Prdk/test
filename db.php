b6afcf625935e265be06d9bf23bdad95549dc347
lexa <alex.n.prdk@gmail.com>
2017-04-21 13:42:54 +0300

    4

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php 
+require_once 'function/query_processing.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Работа с базами данных</title>
+    </head>
+    <body>
+
+    </body>
+</html>
