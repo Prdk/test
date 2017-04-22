bd4b8fda9c3775187ca500898a5ff8bb100c8c85
lexa <alex.n.prdk@gmail.com>
2017-04-22 11:29:43 +0300

    db1

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+header('Content-Type: text/html; charset=utf-8');
 require_once 'function/query_processing.php';
 ?>
 
@@ -6,9 +7,29 @@ require_once 'function/query_processing.php';
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Работа с базами данных</title>
+        <title>Работа с базой данных</title>
     </head>
     <body>
+        <fieldset>
+            <legend align="center">Выборкаю</legend>
+            <form action="db.php" method="POST" name="select">
+                <p>Показать сущиствующие таблицы. <input type="submit" name="select" value="Show_tables"></p>
+                <p>Показать все записи в таблице:
+                    <input type="text" name="show_table" placeholder="имя таблицы">
+                    <input type="submit" name="submit_show_table" value="Show_table">
+                </p>
+            </form>
+        </fieldset>
+        <br/>
+        <fieldset>
+            <legend align="center">Удаление записей из таблицы.</legend>
+            <form action="db.php" method="POST" name="delete">
+                <input type="text" name="name" placeholder="имя">
+                <input type="email" name="email" placeholder="email">
+                <input type="submit" name="del" value="delete">
+            </form>
+
+        </fieldset>
 
     </body>
 </html>
--- function/query_processing.php
+++ function/query_processing.php
@@ -1,3 +1,38 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 
-echo "lkhglkdfnhlbgkdgjnlkbjndlgc";
+echo "шось паказуе <br/>";
+
+echo var_dump($_POST)."<br/>";
+
+$host = 'diy.mysql.ukraine.com.ua';
+$db_name = 'diy_alex';
+$user = 'diy_alex';
+$password = 'kdjgyvx5';
+
+$conn = new PDO("mysql:host=$host;dbname=$db_name", $user, $password);
+
+
+if($conn){
+    $musql_select = "SELECT * FROM users WHERE 1";
+    $res = $conn->query($musql_select)->fetchALL(PDO::FETCH_ASSOC);
+    echo "<pre>";
+    echo var_dump($res);
+    echo "</pre>";
+}else{
+    echo "Нет соединения";
+}
+
+
+function Select(){
+    
+}
+
+function Delete(){
+    
+}
+
+
+function AddNote(){
+    
+}
