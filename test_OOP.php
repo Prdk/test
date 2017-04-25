708cc5439a21eb54cfcf9048becf37aa7c0c4eeb
lexa <alex.n.prdk@gmail.com>
2017-04-25 18:54:03 +0300

    24

--- test_OOP.php
+++ test_OOP.php
@@ -1,39 +1,36 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-class Test {
+
+
+
+$user = new User;
+$user->name = 'lexa';
+$user->age = 34;
+$user->user();
+
+$user2 = clone $user;
+$user2->name = 'lexaxa';
+echo $user->name . "<br/>";
+echo $user2->name . "<br/>";
+
+class User {
 
     public $name;
     public $age;
 
-    public function showName() {
-        echo $this -> name. "Test <br/>";
-    }
-
-    public function getName() {
-        $this->showName();
+    public function user() {
+        echo " код какойта <br/> ";
+        echo $this->name . "<br/>";
+        echo $this->age . "<br/>";
     }
 
 }
 
-class Test2 extends Test{
-    public $name2;
-    public $age2;
-    
-    public function showName2(){
-        echo $this->name. " Test2 <br/> ";
-        Test::getName();
-    }
-    
-}
-
-$test = new Test();
-$test->name = 'user1';
-$test ->getName();
-
-$test2 = new Test2();
-$test2 -> name = 'user2';
-$test2 ->showName2();
+echo "<br/>";
+echo "<pre>";
+var_dump($user);
+echo "</pre>";
 ?>
 
 <!DOCTYPE html>
@@ -46,11 +43,11 @@ $test2 ->showName2();
     <body>
 
 
-        <?php
-        for ($i = 0; $i < 10; $i++) {
-            echo "<br/>";
-        }
-        ?>
+<?php
+for ($i = 0; $i < 10; $i++) {
+    echo "<br/>";
+}
+?>
         <div align="center">
             <a href="index.php"><h2>Венутся на главную</h2></a>
         </div>
