37ae0a10adc299e36a71330e98afc306677526d2
Prdk <alex.n.prdk@gmail.com>
2017-11-27 19:34:51 +0200

    12

---
+++ newTest.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Бред php";
+
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <br>
+        <br>
+        <p><h3>бред</h3></p>
+    </body>
+    
+</html>
+
